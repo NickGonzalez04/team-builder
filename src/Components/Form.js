fe7a6f8c4a48faee3e9e2471beb2bed9b204d378
Nick Gonzalez <gonzaleznick5@gmail.com>
2019-08-20 19:08:51 -0400

    Created Form and TeamList Component

---
+++ src/Components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Form = props =>{
+    return (
+    <form>
+     <input placeholder="name"/>
+     <input placeholder="email"/>
+     <input placeholder="role"/>
+    </form>
+
+    );
+};
+
+
+export default Form;
---
+++ src/Components/TeamList.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+
+const TeamList = (props) =>{
+
+    const [members, setMembers] = useState([]);
+    return (
+<div> 
+
+
+</div>
+    )
+};
+
+export default TeamList;
